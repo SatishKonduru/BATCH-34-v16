72cc9a78dd20eb814b8feb75d019e5dd05f312c5
satish <satishkonduru0225@gmail.com>
2025-11-03 09:54:05 +0530

    canActivate

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-
+  constructor(private _authService: AuthService, private _router: Router) {}
+  logout() {
+    this._authService.logout();
+    this._router.navigate(['/login']);
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+  username = '';
+  password = '';
+  constructor(private _authService: AuthService, private _router: Router) {}
+  onLogin() {
+    console.log('Username: ', this.username);
+    console.log('Password: ', this.password);
+    const success = this._authService.login(this.username, this.password);
+    if (success) {
+      this._router.navigate(['/dashboard']);
+    } else {
+      alert('Invalid Credentials');
+    }
+  }
 }
